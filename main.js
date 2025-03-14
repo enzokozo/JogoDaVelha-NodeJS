ff9bf6d5c9fa436398010a0132a189350cb2000a
Enzo <enzokozonoe@gmail.com>
2025-03-14 14:38:07 -0300

    fiz o exercício a

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ var jogadas = 0;
 var jogador = 'o';
 var vencedor = ' ';
 var fimJogo = false;
-var tab = [3];
-// (a)
+var tab = [];
+
+console.log("Tabuleiro: ")
+for (i = 0; i < 3; i++) {
+    tab[i] = [];
+    for (j = 0; j < 3; j++) {
+        tab[i][j] = ' ';
+    }
+}
+
 while ((jogadas < 9) && (fimJogo == false)) {
     // (b)
     lin = parseInt(prompt(`${jogador} em [linha]: `));

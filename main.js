0dd2e505b5641ce74a031bd663b97b739ace650b
Enzo <enzokozonoe@gmail.com>
2025-03-14 15:01:03 -0300

    #2 fixed

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ for (i = 0; i < 3; i++) {
 
 while ((jogadas < 9) && (fimJogo == false)) {
     // (b)
+    j = 0;
+    for (i = 0; i < 3; i++) {
+        console.log(`[${tab[i][j]}] [${tab[i][j+1]}] [${tab[i][j+2]}]`);
+    }
     lin = parseInt(prompt(`${jogador} em [linha]: `));
     col = parseInt(prompt(`${jogador} em [coluna]: `));
     // (c)
